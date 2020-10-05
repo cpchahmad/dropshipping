0ec5254cd2afdd95d15310d9ecd522b14bb02ddb
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-10-05 20:54:09 +0500

    Orders with bulk fulfillment

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -183,7 +183,7 @@
                         </li>
 
                         <li class="nav-main-item">
-                            <a class="nav-main-link"  aria-haspopup="true" aria-expanded="true" href="/admin/orders?status=unfulfilled">
+                            <a class="nav-main-link"  aria-haspopup="true" aria-expanded="true" href="{{ route('admin.orders') }}">
                                 <i class="nav-main-link-icon si si-bag"></i>
                                 <span class="nav-main-link-name">Orders</span>
                             </a>
