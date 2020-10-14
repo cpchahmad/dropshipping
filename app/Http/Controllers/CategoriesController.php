615e4dbfb886b3a292c3398cb7e170c5fe574b03
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-10-14 21:14:49 +0500

    Final Commit

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use App\Expense;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('expenses.category')->with('categories', Category::paginate(20));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       $this->validate($request, [
+           'name' => 'required'
+       ]);
+
+       $category = new Category();
+       $category->category_name = $request->name;
+       $category->save();
+
+       return redirect()->back()->with('success', 'Category Added Successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'required'
+        ]);
+
+        $category = Category::find($id);
+        $category->category_name = $request->name;
+        $category->save();
+
+        return redirect()->back()->with('success', 'Category Updated Successfully!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if(Expense::where('category', $id)->exists()) {
+            return redirect()->back()->with('error', 'Category cannot be deleted since it is attached to some expense!');
+        }
+        $category = Category::find($id);
+
+        $category->delete();
+
+        return redirect()->back()->with('success', 'Category Deleted Successfully!');
+    }
+}
