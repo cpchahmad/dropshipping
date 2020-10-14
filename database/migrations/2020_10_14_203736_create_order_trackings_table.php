a7490658ecbc78ee4ac75dcb809c37c3919a163c
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-10-15 02:03:26 +0500

    Final Commit

---
+++ database/migrations/2020_10_14_203736_create_order_trackings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderTrackingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_trackings', function (Blueprint $table) {
+            $table->id();
+            $table->string('tracking_number')->nullable();
+            $table->string('tracking_url')->nullable();
+            $table->string('tracking_company')->nullable();
+            $table->bigInteger('shopify_order_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_trackings');
+    }
+}
