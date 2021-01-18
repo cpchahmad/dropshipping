2847d72f7caa1d7427a4dae97a2e289d4477697b
SaadNovaSolutions <saad@tetralogicx.com>
2021-01-18 12:28:51 +0500

    Weebhook Order Update

--- app/Http/Controllers/WordpressController.php
+++ app/Http/Controllers/WordpressController.php
@@ -245,13 +245,13 @@ class WordpressController extends Controller
                     $page++;
                 } while (count($orders) > 0);
 
-                $orders =  json_decode(json_encode($all_orders), FALSE);
-                foreach($orders as $order){
-                    $shop_domain = json_decode(json_encode($order->_links->self['0']->href),True);
-                    $data = explode("wp-json",$shop_domain);
-                    $current_shop_domain =$data['0'];
-                }
-
+//                $orders =  json_decode(json_encode($all_orders), FALSE);
+//                foreach($orders as $order){
+//                    $shop_domain = json_decode(json_encode($order->_links->self['0']->href),True);
+//                    $data = explode("wp-json",$shop_domain);
+//                    $current_shop_domain =$data['0'];
+//                }
+//
 //                $this->worpress_store_order($orders);
 
                 if($orders != null){
